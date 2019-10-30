d2e267d651b0e7a7a20add29447c605cc973f1ee
Badre FOUAD <badre-addine.fouad@outlook.fr>
2019-10-30 16:12:32 +0100

    add exception handling

--- RdpConnector/Program.cs
+++ RdpConnector/Program.cs
@@ -6,30 +6,50 @@ namespace RdpConnector
 {
     class Program
     {
-     
+
         static void Main(string[] args)
         {
+            var rdpFileName = "cpub-FTE_Menu-PRO5-CmsRdsh.rdp";
             //_ = ResponseAsync().Result;
             using (var client = new WebClient())
             {
-                client.DownloadFile("https://clickoncetest1.blob.core.windows.net/clickoncetest/cpub-wordpad-EliorSession-CmsRdsh.rdp", "cpub-wordpad-EliorSession-CmsRdsh.rdp");
+                try
+                {
+                    client.DownloadFile("https://remoteapps.elior.net/RDWeb/Pages/rdp/cpub-FTE_Menu-PRO5-CmsRdsh.rdp", rdpFileName);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    throw;
+                }
             }
 
 
             Process proc = new Process
             {
-                StartInfo = new ProcessStartInfo(Environment.CurrentDirectory+ "\\cpub-wordpad-EliorSession-CmsRdsh.rdp")
+                StartInfo = new ProcessStartInfo(Environment.CurrentDirectory + "\\" + rdpFileName)
             };
 
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            proc.Start();
-            proc.WaitForExit();
+            try
+            {
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                throw;
+            }
             //Process.Start("C:\\Users\\badre-addine.fouad\\Desktop\\gw-vm.rdp");
 
         }
 
-        
+
 
 
 
